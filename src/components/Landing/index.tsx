ed3ed6ac45b510829d6fcc5d64dd7fd56e51af73
SantinoMantineo <santinomantineo@Gmail.com>
2024-07-29 20:09:02 -0300

    landing

--- src/components/Landing/index.tsx
+++ src/components/Landing/index.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import Image from 'next/image';
+import background from "../../../public/images/background.jpg";
+import styles from "./style.module.css"
 
 export default function Landing() {
   return (
-    <h1>HAAA</h1>
+    <div className={styles.main}>
+      <div className={styles.image}>
+        <Image
+            alt="background image"
+            src={background}
+            placeholder="blur"
+            quality={100}
+            priority={true}
+            fill
+            sizes="100dvw"
+            style={{
+              objectFit: 'cover',
+            }}
+          />
+      </div>
+    </div>
   );
 }
